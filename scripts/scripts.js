269cddb335fb36040c646aac5934f35a341cb780
vboxuser <vboxuser@Ubuntu-22.myguest.virtualbox.org>
2023-11-06 14:17:42 -0600

    Updated hero block

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,6 +22,8 @@ const LCP_BLOCKS = []; // add your LCP blocks to the list
 function buildHeroBlock(main) {
   const h1 = main.querySelector('h1');
   const picture = main.querySelector('picture');
+
+  if (main.querySelector('.hero')) return;
   // eslint-disable-next-line no-bitwise
   if (h1 && picture && (h1.compareDocumentPosition(picture) & Node.DOCUMENT_POSITION_PRECEDING)) {
     const section = document.createElement('div');
